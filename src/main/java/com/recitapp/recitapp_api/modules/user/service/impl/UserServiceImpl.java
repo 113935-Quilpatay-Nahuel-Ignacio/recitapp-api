6812fbe13a523ede4c26bba127f654336cf63e0e
Nahuel Ignacio Quilpatay - 113935 <113935@tecnicatura.frc.utn.edu.ar>
2025-05-20 22:21:36 -0300

    delete unused

--- src/main/java/com/recitapp/recitapp_api/modules/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/recitapp/recitapp_api/modules/user/service/impl/UserServiceImpl.java
@@ -10,7 +10,6 @@ import com.recitapp.recitapp_api.modules.user.entity.User;
 import com.recitapp.recitapp_api.modules.user.repository.RoleRepository;
 import com.recitapp.recitapp_api.modules.user.repository.UserRepository;
 import com.recitapp.recitapp_api.modules.user.service.UserService;
-import com.recitapp.recitapp_api.service.FirebaseUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,7 +24,6 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
-    //private final FirebaseUserService firebaseUserService;
 
     @Override
     @Transactional
@@ -42,24 +40,6 @@ public class UserServiceImpl implements UserService {
                 .orElseThrow(() -> new RecitappException("Rol de comprador no encontrado"));
 
         try {
-           /* UserRecord firebaseUser;
-
-            if (registrationDTO.getFirebaseUid() != null && !registrationDTO.getFirebaseUid().isEmpty()) {
-                firebaseUserService.updateUserRole(registrationDTO.getFirebaseUid(), "COMPRADOR");
-                firebaseUser = firebaseUserService.getUser(registrationDTO.getFirebaseUid());
-            } else {
-                firebaseUser = firebaseUserService.createUserWithRole(
-                        registrationDTO.getEmail(),
-                        registrationDTO.getPassword(),
-                        "COMPRADOR"
-                );
-            }
-
-            User user = getUser(registrationDTO, buyerRole, firebaseUser);
-
-            User savedUser = userRepository.save(user);
-            return mapToResponseDTO(savedUser);*/
-
             User user = new User();
             user.setEmail(registrationDTO.getEmail());
             user.setFirstName(registrationDTO.getFirstName());
@@ -80,20 +60,6 @@ public class UserServiceImpl implements UserService {
         }
     }
 
-    /*private static User getUser(UserRegistrationDTO registrationDTO, Role buyerRole, UserRecord firebaseUser) {
-        User user = new User();
-        user.setEmail(registrationDTO.getEmail());
-        user.setFirstName(registrationDTO.getFirstName());
-        user.setLastName(registrationDTO.getLastName());
-        user.setDni(registrationDTO.getDni());
-        user.setCountry(registrationDTO.getCountry());
-        user.setCity(registrationDTO.getCity());
-        user.setRole(buyerRole);
-        user.setAuthMethod(registrationDTO.getFirebaseUid() != null ? "FIREBASE" : "EMAIL");
-        user.setFirebaseUid(firebaseUser.getUid());
-        return user;
-    }*/
-
     @Override
     @Transactional
     public UserResponseDTO createUserWithRole(AdminUserRegistrationDTO registrationDTO) {
@@ -109,12 +75,6 @@ public class UserServiceImpl implements UserService {
                 .orElseThrow(() -> new RecitappException("Rol no encontrado: " + registrationDTO.getRoleName()));
 
         try {
-            /*UserRecord firebaseUser = firebaseUserService.createUserWithRole(
-                    registrationDTO.getEmail(),
-                    registrationDTO.getPassword(),
-                    registrationDTO.getRoleName()
-            );*/
-
             User user = new User();
             user.setEmail(registrationDTO.getEmail());
             user.setFirstName(registrationDTO.getFirstName());
@@ -125,7 +85,6 @@ public class UserServiceImpl implements UserService {
             user.setRole(role);
             user.setAuthMethod("EMAIL");
             user.setPassword(registrationDTO.getPassword());
-            //user.setFirebaseUid(firebaseUser.getUid());
             user.setFirebaseUid("temp-" + UUID.randomUUID());
 
             User savedUser = userRepository.save(user);
